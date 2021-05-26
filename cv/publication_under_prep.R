965fb75012567de450463d1d08cc74da65f6b445
ani-ghosh <ani.biogeo@gmail.com>
2021-05-26 20:28:03 +0300

    update

---
+++ cv/publication_under_prep.R
@@ -0,0 +1,13 @@
+## **Journal Articles (under preparation/submitted)**
+
+> Tiedeman, K., Ghosh, A., Paul, L., Flatnes, JE., Hijmans, RJ., Mapping maize area in Tanzania: a comparison of MODIS, Landsat 8, Sentinel, and Planet satellite data. 
+
+> Ochoa-Brito, JI., Ghosh, A., Hijmans, RJ., Cropland expansion in Ecuador between 2000 and 2016.
+
+> Ochoa-Brito, JI., Ghosh, A., Hijmans, RJ., Jin, Y., Comparing performance of multiple satellite data to map croplands in cloudy and complex terrains.
+
+> Sok, S., Chhinh, N., Ghosh, A., Hijmans, RJ., Sanara, H., Impact of climate change on cultivated rice paddy areas: a comparative study in the Tonle Sap and the Mekong River.
+
+> Lwehabura, J., Stewart, ZP., Rubyogo, JC., Prasad, PVV., Mason, N., Snapp, S., Kilango, M., Ghosh, A., Geospatial analysis for scaling bean technology adoption in Tanzania.
+
+> Ghosh, A., Engle-Stone, R., Charles, D.A., Xiuping, T., Mandel, A., Advancing household survey designs with open source geospatial tools.
